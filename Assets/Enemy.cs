fa52bc736c701b95d31a147ef8a5a1323dd967e0
Kim <lademokim@hotmail.com>
2023-10-26 17:31:40 +0200

    Changed enemy behaviour

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -6,96 +6,56 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
-    [SerializeField]
-    protected float speed;
+    Rigidbody2D myRigidBody;
+    [SerializeField] protected float speed;
 
-    protected Vector3 target;
-    protected Vector3 velocity;
-    protected Vector3 previousPosition;
-    protected bool flipped;
-    
-    [SerializeField]
-    public Transform[] waypoints;
     // Start is called before the first frame update
     void Start()
     {
-        Init();
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
-    public virtual void Init(){
-        target = waypoints[1].position;
-    }
-
-
-    public IEnumerator SetTarget(Vector3 position)
+    //turn when collider detects wall ahead
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        yield return new WaitForSeconds(2f);
-        target = position;
-        FaceTowards(position - transform.position);
-    }
-
-    public void FaceTowards(Vector3 direction)
-    {
-        if (direction.x < 0f)
-            transform.localEulerAngles = new Vector3(0,180,0);
-        else
+        if (other.CompareTag("Wall"))
         {
-            transform.localEulerAngles = new Vector3(0,0,0);
+            transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
         }
     }
 
 
-    public void Movement()
+    //turn when collider doesn't detect floor ahead
+    private void OnTriggerExit2D(Collider2D other)
     {
-        velocity = ((transform.position - previousPosition) / Time.deltaTime);
-        previousPosition = transform.position;
-
-        if(transform.position != target)
+        if (other.CompareTag("Floor"))
         {
-            transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
+            transform.localScale = new Vector2(-(Mathf.Sign(myRigidBody.velocity.x)), 1f);
+        }
+    }
+
+    private bool isFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void Movement()
+    {
+        if (isFacingRight())
+        {
+            myRigidBody.velocity = new Vector2(speed, 0f);
         }
         else
         {
-            // Toggle between waypoints
-            if (target == waypoints[0].position)
-            {
-                StartCoroutine("SetTarget", waypoints[1].position);
-            }
-            else
-            {
-                StartCoroutine("SetTarget", waypoints[0].position);
-            }
-        
-        
-    
-
-
-            // if (target == waypoints[0].position)
-            // {
-            //     if (flipped)
-            //     {
-            //         flipped = !flipped;
-            //         StartCoroutine("SetTarget", waypoints[1].position);
-            //     }
-            // }
-            // else
-            // {
-            //     if(!flipped)
-            //     {
-            //         flipped = !flipped;
-            //         StartCoroutine("SetTarget", waypoints[0].position);
-            //     }
-            // }
-
-            
+            myRigidBody.velocity = new Vector2(-speed, 0f);
         }
-
-
     }
+
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+
     }
 }
