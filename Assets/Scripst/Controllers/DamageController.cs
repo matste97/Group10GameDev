5cb616557c9ee445fe58996f0ddbfac6f59c7bd8
Filip Berg Vindsetmo <filip@vindsetmo.no>
2023-11-08 15:41:41 +0100

    Player health
    Health and damage controller + health bar

---
+++ Assets/Scripst/Controllers/DamageController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    [SerializeField] private int damage;
+
+    [SerializeField] private HealthController _healthController;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Damage();
+        }
+    }
+
+    void Damage()
+    {
+        _healthController.playerHealth = _healthController.playerHealth - damage;
+        _healthController.UpdateHealth();
+
+    }
+
+
+}
