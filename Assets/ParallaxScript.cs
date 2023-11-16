41082b6ea6ca82ca5ea9711a4259d2a42a590543
Mats Hag Tellefsen <matste@stud.ntnu.no>
2023-11-16 19:52:39 +0100

    i have perfected the art of parallax backgrounds

--- Assets/ParallaxScript.cs
+++ Assets/ParallaxScript.cs
@@ -1,45 +1,23 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-namespace _Scripts
+public class Parallax : MonoBehaviour
 {
-    public class ParallaxEffect : MonoBehaviour
+    private float length, startpos;
+    public GameObject cam;
+    public float parallexEffect;
+    void Start()
     {
-        private float _startingPos, //This is the starting position of the sprites.
-            _lengthOfSprite; //This is the length of the sprites.
-        public float AmountOfParallax; //This is amount of parallax scroll. 
-        public Camera MainCamera; //Reference of the camera.
-
-
-
-        private void Start()
-        {
-            //Getting the starting X position of sprite.
-            _startingPos = transform.position.x;
-            //Getting the length of the sprites.
-            _lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
-        }
-
-
-
-        private void Update()
-        {
-            Vector3 Position = MainCamera.transform.position;
-            float Temp = Position.x * (1 - AmountOfParallax);
-            float Distance = Position.x * AmountOfParallax;
-
-            Vector3 NewPosition = new Vector3(_startingPos + Distance, transform.position.y, transform.position.z);
-
-            transform.position = NewPosition;
-
-            if (Temp > _startingPos + (_lengthOfSprite / 2))
-            {
-                _startingPos += _lengthOfSprite;
-            }
-            else if (Temp < _startingPos - (_lengthOfSprite / 2))
-            {
-                _startingPos -= _lengthOfSprite;
-            }
-        }
+        startpos = transform.position.x;
+        length = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+    void Update()
+    {
+        float temp = (cam.transform.position.x * (1 - parallexEffect));
+        float dist = (cam.transform.position.x * parallexEffect);
+        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        if (temp > startpos + length) startpos += length;
+        else if (temp < startpos - length) startpos -= length;
     }
 }
