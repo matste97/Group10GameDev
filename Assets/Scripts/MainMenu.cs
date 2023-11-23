fe3640438e05258da163fdc4c98cc75c971ff934
Filip Berg Vindsetmo <filip@vindsetmo.no>
2023-11-23 19:11:03 +0100

    Enkel fungerend ui

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    
-    public void PlayGame()
+    public void LoadScene(string sceneName)
     {
-       // SceneManager.LoadSceneAsyns(1);
+        SceneManager.LoadScene(sceneName);
     }
 
+    public void QuitGame()
+    {
+        // Application.Quit() does not work in the editor
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game in the ediotor
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
 }
